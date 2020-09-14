c5f4a00fa78e2fa8f34ad162dbc3836ad991fb63
Felipe Santos <info@felipels.com>
2020-09-14 23:53:44 +0100

    Adding featured images for the post list

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<?php get_header(); ?>     
+<?php get_header(); ?>  
+
         <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : ?>
                 <?php the_post(); ?>
                 <div class="post">
                     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php the_post_thumbnail(); ?>
                     <?php the_excerpt(); ?>
                 </div>
             <?php endwhile; ?>
