bcb1f56292bac34d6e077dfb4720113c8013af3f
Felipe Santos <info@felipels.com>
2020-09-14 23:50:08 +0100

    Featured image support

--- functions.php
+++ functions.php
@@ -19,6 +19,12 @@ function blindtomato_custom_logo_setup() {
 
 add_action( 'after_setup_theme', 'blindtomato_custom_logo_setup' );
 
+function blindtomato_featured_image_setup() {
+    add_theme_support( 'post-thumbnails' );
+   }
+
+add_action( 'after_setup_theme', 'blindtomato_featured_image_setup' );
+
 function blindtomato_register_sidebar() {
     register_sidebar( 
         array(
